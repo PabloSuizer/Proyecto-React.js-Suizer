0e45f1bd607c5453afabff1bd2353620ced25ed7
Juan Pablo Suizer <suizerpablo@gmail.com>
2024-02-29 19:53:23 -0300

    Entrega final

--- src/components/AddButton/AddButton.jsx
+++ src/components/AddButton/AddButton.jsx
@@ -37,16 +37,16 @@ const AddButton = ({stock, onAdd}) => {
         <>
             <div className="row my-1">
                 <div className="col-md-4">
-                    <div className="btn-group" role="group" aria-label="Basic example">
-                        <button type="button" className="btn btn-warning" onClick={decrementar}>-</button>
-                        <button type="button" className="btn btn-warning">{counter}</button>
-                        <button type="button" className="btn btn-warning" onClick={incrementar}>+</button>
+                    <div className="btn-group" role="group" >
+                        <button type="button" className="btn btn-primary text-dark" onClick={decrementar}>-</button>
+                        <button type="button" className="btn btn-primary text-dark">{counter}</button>
+                        <button type="button" className="btn btn-primary text-dark" onClick={incrementar}>+</button>
                     </div>
                 </div>
             </div>
             <div className="row my-1">
                 <div className="col-md-5">
-                    {itemAdded ? <Link to={"/cart"} className="btn btn-warning">Terminar Mi Compra</Link> : <button type="button" className="btn btn-warning" onClick={addToCart}>Agregar al Carrito</button>}
+                    {itemAdded ? <Link to={"/cart"} className="btn btn-primary ">Terminar Mi Compra</Link> : <button type="button" className="btn btn-primary text-dark" onClick={addToCart}>Agregar al Carrito</button>}
                 </div>
             </div>
         </>
--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import iconCart from "../../assets/cart.svg";
+import iconCart from "../../assets/cart-outline.svg";
 import { CartContext } from "../Context/CartContext";
 import { Link } from "react-router-dom";
 
@@ -8,7 +8,7 @@ const CartWidget = () => {
 
     return (
         CantTotalProductos() > 0 ? <Link to={"/cart"} className="btn btn-warning position-relative">
-            <img src={iconCart} alt="Carrito" width={24} />
+            <img src={iconCart} alt="Carrito" />
             <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{CantTotalProductos()}</span>
         </Link> : ""
     )
